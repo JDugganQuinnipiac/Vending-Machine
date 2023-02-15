d298fd2d12e8d36ba73d1e7387e2fc8286f4b8ae
JDugganQuinnipiac <jduggan@quinnipiac.edu>
2023-02-14 19:44:20 -0500

    creating list of snacks and their prices

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -2,6 +2,14 @@ public class VendingMachine
 {
     public static void main(String[] args) throws Exception 
     {
-    
+        double CookiesPrice = 1.50;
+        double DoritosPrice = 2.50;
+        double WaterPrice = 1.25;
+        double MentosPrice = 0.75;
+        double AlmondJoysPrice = 1.25;
+        double MandMsPrice = 1.00;
+        double SkittlesPrice = 1.00;
+        double SunchipsPrice = 1.50;
+        double LaysPrice = 0.75;
     }
 }

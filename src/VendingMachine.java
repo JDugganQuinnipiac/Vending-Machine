d427856c5d6bd3fa6c4f198d524e53d3374f8501
AnzilottiChristopher <anzilottichr@gmail.com>
2023-02-14 20:23:19 -0500

    Added welcome message and getting user input

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class VendingMachine 
 {
     public static void main(String[] args) throws Exception 
     {
-        double CookiesPrice = 1.50;
-        double DoritosPrice = 2.50;
-        double WaterPrice = 1.25;
-        double MentosPrice = 0.75;
-        double AlmondJoysPrice = 1.25;
+        Scanner scan = new Scanner(System.in);
+        //Variable Prices
+        double cookiesPrice = 1.50;
+        double doritosPrice = 2.50;
+        double waterPrice = 1.25;
+        double mentosPrice = 0.75;
+        double almondJoysPrice = 1.25;
         double MandMsPrice = 1.00;
-        double SkittlesPrice = 1.00;
-        double SunchipsPrice = 1.50;
-        double LaysPrice = 0.75;
+        double skittlesPrice = 1.00;
+        double sunchipsPrice = 1.50;
+        double laysPrice = 0.75;
+
+        //Welcom Message and Input
+        System.out.println("Welcome, please enter corresponding number for item wanted:\n 1.Cookies($1.50) 2.Doritos($2.50) 3.Water($1.25) 4.Mentos($0.75) 5.AlmondJoys($1.25)\n 6.M&Ms($1.00) 7.Skittles($1.00) 8.Sunchips($1.50) 9.Lays($0.75): ");
+        int check = scan.nextInt();
     }
 }

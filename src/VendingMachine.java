e8ab6969cad19f2927a265d1ee4a817a40dd5e3f
JDugganQuinnipiac <jduggan@quinnipiac.edu>
2023-02-15 10:38:06 -0500

    added change given for each purchase and payment. Errors such as, not enough money and invalid selections handled.

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -11,7 +11,7 @@ public class VendingMachine
         double waterPrice = 1.25;
         double mentosPrice = 0.75;
         double almondJoysPrice = 1.25;
-        double MandMsPrice = 1.00;
+        double mAndMsPrice = 1.00;
         double skittlesPrice = 1.00;
         double sunchipsPrice = 1.50;
         double laysPrice = 0.75;
@@ -19,5 +19,90 @@ public class VendingMachine
         //Welcom Message and Input
         System.out.println("Welcome, please enter corresponding number for item wanted:\n 1.Cookies($1.50) 2.Doritos($2.50) 3.Water($1.25) 4.Mentos($0.75) 5.AlmondJoys($1.25)\n 6.M&Ms($1.00) 7.Skittles($1.00) 8.Sunchips($1.50) 9.Lays($0.75): ");
         int check = scan.nextInt();
+
+        //Prompt user to enter money amount
+        System.out.println("Please insert the amount of money you are paying. Only $1 bills are accepted");
+        int payment = scan.nextInt();
+
+
+        //Change for each purchase
+        double change = 0;
+        
+        if(check == 1){
+            if(payment < 1.5){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - cookiesPrice;
+            }
+        }else if(check == 2){
+            if(payment < 2.5){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - doritosPrice;
+            }
+
+        }else if(check == 3){
+            if(payment < 1.25){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - waterPrice;
+            }
+
+        }else if(check == 4){
+            if(payment < 0.75){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - mentosPrice;
+            }
+
+        }else if(check == 5){
+            if(payment < 1.25){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - almondJoysPrice;
+            }
+
+        }else if(check == 6){
+            if(payment < 1){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - mAndMsPrice;
+            }
+
+        }else if(check == 7){
+            if(payment < 1){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - skittlesPrice;
+            }
+
+        }else if(check == 8){
+            if(payment < 1.5){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - sunchipsPrice;
+            }
+
+        }else if(check == 9){
+            if(payment < 0.75){
+                System.out.println("Sorry not enough money was entered. Your money will be given back. Please try again");
+                change = payment;
+            }else{
+                change = payment - laysPrice;
+            }
+        }else{
+            System.out.println("Sorry, invalid selection. Please try again");
+            change = payment;
+        }
+
+        System.out.println("Your change is $" + change);
     }
 }
